fd954b115482488cceca8bcd9c7c9455c6f4b438
Matthew Coyle <matthew@coyle.fr>
2018-11-09 14:48:45 +0100

    TP5 table declarations

--- TP/TP5.sql
+++ TP/TP5.sql
@@ -1,6 +1,9 @@
-drop table societe;
-drop table campagne;
+drop table programmation;
+drop table tarif;
 drop table spot;
+drop table campagne;
+drop table societe;
+
 
 create table societe(
 	CodeSoc numeric, 
@@ -12,7 +15,7 @@ create table societe(
 create table campagne(
 	CodeCamp varchar(5) primary key,
 	Titre varchar,
-	CodeSoc numeric references societe(CodeSoc) on delete cascade,
+	CodeSoc numeric references societe(CodeSoc),
 	Produit varchar,
 	Cible varchar(4),
 	constraint NomCible
@@ -25,19 +28,25 @@ create table spot(
 	Duree numeric,
 	CodeCamp varchar(5),
 	primary key (CodeSpot),
-	foreign key (CodeCamp) references campagne(CodeCamp) on delete cascade;
+	foreign key (CodeCamp) references campagne(CodeCamp)
 );
 
 create table tarif(
 	Media varchar,
 	Moment varchar,
 	prix numeric,
+	constraint Medias
+	check(Media='TF1' or Media='FRANCE2' or Media='FRANCE3' or Media='CANAL+' or Media='FRANCE5' or Media='M6' or Media='ARTE'),
+	constraint Moments
+	check(Moment='JOUR' or Moment='DEBSOIR' or Moment='SOIR' or Moment='FINS'),
 	primary key(Media, Moment)
 );
 
 create table programmation(
-	CodeSpot char(3),
-	Media varchar,
-	Moment varchar,
-	Datep date
+	CodeSpot char(3) references spot(CodeSpot),
+	Media varchar, --references tarif(Media),
+	Moment varchar, --references tarif(Moment),
+	Datep date,
+	primary key (CodeSpot,Media,Moment,Datep),
+	foreign key (Media,Moment) references tarif(Media,Moment)	
 );
