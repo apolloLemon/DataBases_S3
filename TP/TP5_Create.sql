2a4d2970c5439ba59315b8851bb610f104ec012b
Matthew Coyle <matthew@coyle.fr>
2018-11-16 15:04:00 +0100

    TP5_Modify

--- TP/TP5_Create.sql
+++ TP/TP5_Create.sql
@@ -50,3 +50,35 @@ create table programmation(
 	primary key (CodeSpot,Media,Moment,Datep),
 	foreign key (Media,Moment) references tarif(Media,Moment)	
 );
+
+insert into societe values
+	(101,'Nescafe','523 rue des grains Paris 13'),
+	(102,'CIG','5 rue du brigand Paris 15'),
+	(120,'Peugeot','36 avenue de la routourne Angers');
+
+insert into campagne values
+	('NE025','Nespresso-Clooney',101,'Cafe','MM50'),
+	('P208P','208-Pinocchio',120,'Voiture','TOUS'),
+	('BA75','Le monde bouge',102,'Banque','MM50');
+
+insert into spot values 
+	('NS1','Rencontre au bar',135,'NE025'),
+	('NS2','Soiree',115,'NE025'),
+	('P12','Mobile',60,'BA75'),
+	('P13','Premier emploi',75,'BA75'),
+	('P14','Sejour etranger',45,'BA75');
+
+insert into tarif values
+	('TF1','SOIR',500),
+	('TF1','DEBSOIR',400),
+	('FRANCE2','JOUR',50),
+	('FRANCE3','JOUR',50),
+	('FRANCE3','DEBSOIR',200);
+
+insert into programmation values
+	('NS1','TF1','SOIR','2018-02-17'),
+	('NS1','FRANCE3','JOUR','2020-02-17'),
+	('NS2','TF1','DEBSOIR','2020-02-17'),
+	('P12','TF1','DEBSOIR','2019-02-17'),
+	('P13','TF1','DEBSOIR','2020-02-17'),
+	('P13','FRANCE3','DEBSOIR','2020-02-17');
---
+++ TP/TP5_Modify.sql
@@ -0,0 +1,57 @@
+/*
+--2 
+select * from societe;
+select * from campagne;
+select * from spot;
+select * from tarif;
+select * from programmation;
+
+--3
+alter table tarif drop constraint Medias;
+alter table tarif add constraint Medias check (Media='TF1' or Media='FRANCE2' or Media='FRANCE3' or Media='CANAL+' or Media='FRANCE5' or Media='M6' or Media='ARTE' or Media='W9' or Media='NT1');
+insert into tarif values 
+	('W9','FINS',50),
+
+	('NT1','FINS',80);
+
+--4
+update spot
+	set libelle = 'Rencontre en soiree'
+	where codespot = 'NS2';
+
+--5
+insert into societe values 
+	(100,'Adidas');
+
+--7
+insert into spot values 
+	('NS3','A la maison',null,
+		(select codecamp 
+		from campagne 
+		where titre = 'Nespresso-Clooney'));
+
+--6
+insert into programmation values 
+	('NS3','TF1','SOIR','2017-02-18');
+
+--8
+create table SPOT2 as (
+	select libelle, duree from spot
+);
+-- PRE 9
+drop table client;
+CREATE TABLE CLIENT (NumC CHAR(6)  NOT NULL, NomC VARCHAR(20) NOT NULL, AnneeNaissC INTEGER, VilleC VARCHAR(20) NOT NULL, PRIMARY KEY (NumC)); 
+INSERT INTO CLIENT VALUES 
+('CLI001', 'Jean', 1975, '75006 Paris'),
+('CLI002', 'Paul', 1978, '75003 Paris'),
+('CLI003', 'Vincent', 1984, '94200 Evry'),
+('CLI004', 'Pierre', 1970, '92400 Courbevoie'),
+('CLI005', 'Daniel', 1963, '44112 Nantes');
+
+-- 9 */
+create table employe as (
+	(select NumC,codesoc 
+		from CLIENT,societe 
+		where (NomC = 'Daniel' and nom = 'Nescafe') 
+		or (VilleC like '%Paris' and nom = 'Peugeot'))
+);
