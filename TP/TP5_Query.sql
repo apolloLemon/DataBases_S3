19ae31801db8b6dc3adf4a769ebec964dab5a1d5
Matthew Coyle <matthew@coyle.fr>
2018-11-23 13:40:08 +0100

    TP5 Query start

--- TP/TP5_Query.sql
+++ TP/TP5_Query.sql
@@ -5,8 +5,16 @@ from programmation
 where Datep = '2017-02-20'
 and Media = 'TF1';
 
---2 */
+--2
+select libelle, duree
+from spot natural join campagne
+where titre = 'Nespresso-Clooney';
+
 --3 */
+select codespot, libelle
+
+order by libelle;
+
 --4 */
 --5 */
 --6 */
