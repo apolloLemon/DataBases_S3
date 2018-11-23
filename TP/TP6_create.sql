68f2c4b02c98441facae6b99319b21b54c0811ff
Matthew Coyle <matthew@coyle.fr>
2018-11-23 14:25:25 +0100

    TP6.2-3

--- TP/TP6_create.sql
+++ TP/TP6_create.sql
@@ -1,8 +1,11 @@
 create sequence ETi minvalue 1111;
+create sequence Ci;
 
 create table groupe(
 	nomgroupe char(5) primary key,
-	intitulegroupe varchar
+	intitulegroupe varchar,
+	constraint grpname
+	check nomgroupe in ('A1','A2','A3','A4','A5','A6')
 );
 
 create table etudiant(
@@ -11,7 +14,9 @@ create table etudiant(
 	prenomet char(30),
 	adret char(50),
 	datnais date,
-	nomgr char references group(nomgroupe)
+	nomgr char, references group(nomgroupe) on delete cascade on update cascade,
+	constraint agerange
+	check (extract(year from age(datnais)) between 16 and 40)
 );
 
 create table enseignant(
@@ -20,21 +25,27 @@ create table enseignant(
 );
 
 create table matiere(
-	nommat varchar primary key,
+	nommat varchar(15) primary key,
 	coefmat numeric,
-	numens numeric references enseignant(numens)
+	numens numeric references enseignant(numens) on delete cascade on update cascade,
+	constraint coefrange
+	check coef between 1 and 8
 );
 
 create table controle(
 	numcont numeric primary key,
-	nommat varchar references matiere(nommat),
+	nommat varchar references matiere(nommat) on delete cascade on update cascade,
 	datcont date,
 	coef numeric,
+	constraint coefrange
+	check coef between 1 and 3
 );
 
 create table passcont(
-	numet numeric references etudiant(numet),
-	numcont numeric references controle(numcont),
+	numet numeric references etudiant(numet) on delete cascade on update cascade,
+	numcont numeric references controle(numcont) on delete cascade on update cascade,
 	note numeric,
-	primary key (numet,numcont)
+	primary key (numet,numcont),
+	constraint noterange
+	check coef between 0 and 20
 );
